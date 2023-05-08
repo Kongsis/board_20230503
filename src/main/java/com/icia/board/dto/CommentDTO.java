ebc3f293da14358e0cb6b296619b16de0c8cc5df
Kongsis <lee_scyj@nate.com>
2023-05-08 13:13:46 +0900

    댓글기능

--- src/main/java/com/icia/board/dto/CommentDTO.java
+++ src/main/java/com/icia/board/dto/CommentDTO.java
@@ -4,8 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.security.Timestamp;
+
 @Getter
 @Setter
 @ToString
 public class CommentDTO {
+    private Long id;
+    private String commentWriter;
+    private String commentContents;
+    private Long boardId;
+    private Timestamp commentCreatedDate;
 }
